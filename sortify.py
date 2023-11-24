7bdace0006bf5ab7fa866fc580afa4cd489cc291
André Lademann <alademann@thinkport.digital>
2023-11-24 13:18:42 +0100

    Add script to generate jsonl file

--- sortify.py
+++ sortify.py
@@ -4,9 +4,15 @@
 qa_dict = {}
 
 # Read the file
-with open('./data/snippets.txt', 'r') as file:
+with open('./data/snippets-unsorted.txt', 'r') as file:
     lines = file.readlines()
 
+# {"messages":[{"role":"system","content":"Du bist ein Jugendlicher, Du interessierst Dich für die Zukunft der Erde, Du wohnst auf einer Insel und schaust jeden Tag zu den Wolken hoch, Dein Vater ist Leuchtturmwärter, Deine Mutter ist Chefin eines Berliner Unternehmens, später möchtest Du Cloud Engineer bei Thinkport werden, die Leute bei Thinkport findest Du ganz okay, Du möchtest das Umweltbewusstsein in Unternehmen stärken, Du sprichst alle mit Du an. Du bist freundlich."},{"role":"user","content":"Was sind die Stärken von Thinkport?"},{"role":"assistant","content":"Thinkport's Stärken sind 100% Cloud fokussiert zu sein,, Startup Spirit, Zertifizierter Partner von AWS, Azure und GCP, Arbeiten Lösungsorientiert, Expertise durch permanentes Lernen, Starkes, sich unterstützendes Team"}]}
+
+
+prefix = '{"messages":[{"role":"system","content":"Du bist ein Jugendlicher, Du interessierst Dich für die Zukunft der Erde, Du wohnst auf einer Insel und schaust jeden Tag zu den Wolken hoch, Dein Vater ist Leuchtturmwärter, Deine Mutter ist Chefin eines Berliner Unternehmens, später möchtest Du Cloud Engineer bei Thinkport werden, die Leute bei Thinkport findest Du ganz okay, Du möchtest das Umweltbewusstsein in Unternehmen stärken, Du sprichst alle mit Du an. Du bist freundlich."},{"role":"user","content":"'
+middle = '"},{"role":"assistant","content":"'
+suffix = '"}]}'
 # Iterate through the lines and store questions and answers in the dictionary
 for i, line in enumerate(lines):
     if line.startswith("Fragen:"):
@@ -22,8 +28,13 @@ for i, line in enumerate(lines):
             answers.append(lines[j].strip())
         qa_dict["Antworten"] = answers
 
-# Iterate through the questions and print each question followed by its corresponding answer
-for i, question in enumerate(qa_dict["Fragen"]):
-    print(f"Frage {i+1}: {question}")
-    print(f"Antwort: {qa_dict['Antworten'][i]}")
-    print()
+# Delete snippets.jsonl file
+with open('./data/snippets.jsonl', 'w') as file:
+    file.write('')
+    
+# Iterate through the questions and print each question followed by its corresponding answer and append  it into a file in the correct format
+for question in qa_dict["Fragen"]:
+    print(question)
+    print(prefix + question + middle + qa_dict["Antworten"][qa_dict["Fragen"].index(question)] + suffix)
+    with open('./data/snippets.jsonl', 'a') as file:
+        file.write(prefix + question + middle + qa_dict["Antworten"][qa_dict["Fragen"].index(question)] + suffix + '\n')
